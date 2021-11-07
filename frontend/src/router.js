f63799bce8450967fd0415e172fd2420bc9bd327
junhong91 <junhong199@gmail.com>
2021-11-07 14:16:29 +0900

    Add vue router

---
+++ frontend/src/router.js
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+const router = new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: () => import("@/views/home.vue"),
+    },
+  ],
+});
+
+const originalPush = Router.prototype.push;
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch((err) => {
+    if (err.name !== "NavigationDuplicated") throw err;
+  });
+};
+
+export default router;
