27f1de6f6cb4b16a154d319060dacdd8130a2b97
junhong91 <junhong199@gmail.com>
2021-11-07 15:50:07 +0900

    Add Search Bar

--- frontend/src/components/map.vue
+++ frontend/src/components/map.vue
@@ -1,9 +1,28 @@
 <template>
-  <div>Map</div>
+  <div>
+    <!-- Search Bar -->
+    <v-text-field
+      filled
+      dense
+      placeholder="Search"
+      v-model="search"
+      :append-icon="'mdi-magnify'"
+      @click:append="getEstates"
+    >
+    </v-text-field>
+  </div>
 </template>
 
 <script>
 export default {
   components: {},
+  data: () => ({
+    search: "",
+  }),
+  methods: {
+    getEstates() {
+      console.log("Get Estate!");
+    },
+  },
 };
 </script>
