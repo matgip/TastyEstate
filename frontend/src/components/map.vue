8cddfc5b9a0c2a784d05e1c2e8101f025810cafe
junhong91 <junhong199@gmail.com>
2021-11-07 17:07:30 +0900

    Add nav-bar and v-card sample

--- frontend/src/components/map.vue
+++ frontend/src/components/map.vue
@@ -10,6 +10,31 @@
       @click:append="getEstates"
     >
     </v-text-field>
+
+    <!-- Navigation Drawer -->
+    <v-navigation-drawer absolute permanent left height="100vh">
+      <v-card>
+        <v-list-item>
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/women/81.jpg" />
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>차지연</v-list-item-title>
+            <v-list-item-subtitle>8시간 전</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>매매 100억</v-list-item-title>
+            <v-list-item-subtitle>대지 141평</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card >
+    </v-navigation-drawer>
   </div>
 </template>
 
@@ -18,6 +43,7 @@ export default {
   components: {},
   data: () => ({
     search: "",
+    show: false,
   }),
   methods: {
     getEstates() {
