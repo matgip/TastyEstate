b40a2497e97d392542dcd5d26c7e2c3f20d9c257
junhong91 <junhong199@gmail.com>
2021-11-07 20:36:39 +0900

    Refactoring: Divide feed-cards into sub components

--- frontend/src/components/mapNavBar.vue
+++ frontend/src/components/mapNavBar.vue
@@ -1,43 +1,21 @@
 <template>
   <div>
     <!-- Navigation Drawer -->
-    <v-navigation-drawer absolute permanent left height="100vh">
+    <v-navigation-drawer absolute permanent left height="100vh" width="40vh">
       <MapSearch />
-
-      <!-- Example v-card: estate information to sell -->
-      <v-card>
-        <!-- Detail information of person who sell the estate -->
-        <v-list-item>
-          <v-list-item-avatar>
-            <img src="https://randomuser.me/api/portraits/women/81.jpg" />
-          </v-list-item-avatar>
-
-          <v-list-item-content>
-            <v-list-item-title>차지연</v-list-item-title>
-            <v-list-item-subtitle>8시간 전</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-divider></v-divider>
-
-        <!-- Detail information of estate such as price or area -->
-        <v-list-item>
-          <v-list-item-content>
-            <v-list-item-title>매매 100억</v-list-item-title>
-            <v-list-item-subtitle>대지 141평</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-card>
+      <MapFeedCards />
     </v-navigation-drawer>
   </div>
 </template>
 
 <script>
 import MapSearch from "./mapNavBarSearch.vue";
+import MapFeedCards from "./mapNavBarFeedCards.vue";
 
 export default {
   components: {
     MapSearch,
+    MapFeedCards,
   },
 };
 </script>
---
+++ frontend/src/components/mapNavBarFeedCards.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-card>
+      <!-- Detail information of person who sell the estate -->
+      <v-list-item>
+        <v-list-item-avatar>
+          <img src="https://randomuser.me/api/portraits/women/81.jpg" />
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>차지연</v-list-item-title>
+          <v-list-item-subtitle>8시간 전</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <!-- Detail information of estate such as price or area -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>매매 100억</v-list-item-title>
+          <v-list-item-subtitle>대지 141평</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
