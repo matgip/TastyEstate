2ecd08bce8f216221c68f66589f384a378addbfa
junhong91 <junhong199@gmail.com>
2022-02-12 00:57:34 +0900

    Upload Image to local storage

---
+++ backend/routes/upload.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const multer = require("multer");
+
+const router = express.Router();
+
+const fs = require("fs");
+const path = require("path");
+const uploadPath = path.join(__dirname, "..", "uploads");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    // const filename = (new Date()).getTime().toString(36) + Math.random().toString(36).slice(2);
+    // const filename = file.filename;
+    const filename = file.originalname;
+    cb(null, filename);
+  },
+});
+const upload = multer({ storage: storage });
+
+router.post("/", upload.single("file"), function (req, res, next) {
+  console.log("UPLOAD " + req.file.filename);
+  res.json({ my_key: req.file.filename }); // you can send any arbitrary data to client which will be saved in fileRecord.upload key in client, and will be sent back to server at update/delete request
+});
+
+router.put("/", function (req, res, next) {
+  let response = {};
+  if (req.body && req.body.my_key && req.body.filename) {
+    console.log("UPDATE UPLOAD " + req.body.my_key);
+    fs.rename(path.join(uploadPath, req.body.my_key), path.join(uploadPath, req.body.filename), (err) => {
+      if (err) throw err;
+      console.log("Rename complete!", req.body.my_key, req.body.filename);
+      response.my_key = req.body.filename;
+      console.log("Rename complete!", response);
+      res.json(response);
+    });
+  }
+});
+
+router.delete("/", function (req, res, next) {
+  let response = {};
+  if (req.body && req.body.my_key) {
+    console.log("DELETE UPLOAD " + req.body.my_key);
+    fs.unlink(path.join(uploadPath, req.body.my_key), (err) => {
+      if (err) throw err;
+      console.log(req.body.my_key + " was deleted");
+      response.deleted = true;
+      res.json(response);
+    });
+  }
+});
+
+module.exports = router;
---
+++ frontend/src/components/Map/MapCardFileUpload.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <VueFileAgent
+      ref="vueFileAgent"
+      :uploadUrl="uploadUrl"
+      :multiple="true"
+      :deletable="true"
+      :meta="true"
+      :helpText="'부동산 이미지를 자유롭게 업로드해주세요!'"
+      @select="filesSelected($event)"
+      @delete="fileDeleted($event)"
+      v-model="fileRecords"
+    ></VueFileAgent>
+  </div>
+</template>
+
+<script>
+// Reference: https://madewithvuejs.com/vue-file-agent
+export default {
+  data() {
+    return {
+      fileRecords: [],
+      uploadUrl: "http://localhost:3000/upload",
+      uploadHeaders: { "X-Test-Header": "vue-file-agent" },
+      fileRecordsForUpload: [], // maintain an upload queue
+    };
+  },
+
+  methods: {
+    uploadFiles() {
+      // Using the default uploader. You may use another uploader instead.
+      this.$refs.vueFileAgent.upload(this.uploadUrl, this.uploadHeaders, this.fileRecordsForUpload);
+      this.fileRecordsForUpload = [];
+    },
+    deleteUploadedFile(fileRecord) {
+      // Using the default uploader. You may use another uploader instead.
+      this.$refs.vueFileAgent.deleteUpload(this.uploadUrl, this.uploadHeaders, fileRecord);
+    },
+    filesSelected(fileRecordsNewlySelected) {
+      const validFileRecords = fileRecordsNewlySelected.filter((fileRecord) => !fileRecord.error);
+      this.fileRecordsForUpload = this.fileRecordsForUpload.concat(validFileRecords);
+    },
+    fileDeleted(fileRecord) {
+      if (!confirm("정말로 이미지를 삭제하시겠습니까?")) return;
+
+      const i = this.fileRecordsForUpload.indexOf(fileRecord);
+      if (i !== -1) {
+        this.fileRecordsForUpload.splice(i, 1);
+      } else {
+        this.deleteUploadedFile(fileRecord);
+      }
+    },
+  },
+};
+</script>
