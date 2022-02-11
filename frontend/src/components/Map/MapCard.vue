4337e01da8b83af4c492cc0aa4acb7d208762f49
junhong91 <junhong199@gmail.com>
2022-02-12 03:33:32 +0900

    Divide MapCard.vue file

--- frontend/src/components/Map/MapCard.vue
+++ frontend/src/components/Map/MapCard.vue
@@ -2,49 +2,21 @@
   <div>
     <v-card v-if="Object.keys(estate).length !== 0">
       <MapCardImageUpload v-bind:estateID="estate.id" />
+
       <v-card-title>{{ estate.place_name }}</v-card-title>
 
-      <!-- Likes and Stars -->
-      <v-card-text>
-        <v-row align="center" class="mx-0">
-          <v-rating :value="4.5" size="18" color="amber" dense readonly half-increments></v-rating>
-          <div class="grey--text ms-4">
-            4.5 (413)
-          </div>
-        </v-row>
-      </v-card-text>
+      <MapCardRating v-bind:rating="4.9" v-bind:likes="512" />
 
       <v-divider class="mx-4" />
 
       <v-card-text>
-        <v-btn class="ma-2" color="deep-orange" x-small outlined rounded>
-          <v-icon left x-small>
-            fas fa-heart
-          </v-icon>
-          좋아요(413)
-        </v-btn>
-
-        <v-btn class="ma-2" color="deep-orange" x-small outlined rounded>
-          <v-icon left x-small>
-            fas fa-edit
-          </v-icon>
-          평가하기
-        </v-btn>
+        <MapCardLikes v-bind:likes="512" />
+        <MapCardEval />
       </v-card-text>
 
       <v-divider class="mx-4" />
 
-      <!-- Real Estate Detail Infos -->
-      <v-card-text>
-        <div>
-          <v-icon x-small>fas fa-map-marker-alt</v-icon>
-          {{ estate.address_name }}
-        </div>
-        <div>
-          <v-icon x-small>fas fa-book</v-icon>
-          {{ estate.phone }}
-        </div>
-      </v-card-text>
+      <MapCardEstateInfo v-bind:estateInfo="estate" />
     </v-card>
   </div>
 </template>
@@ -52,10 +24,18 @@
 <script>
 import { mapGetters } from "vuex";
 import MapCardImageUpload from "./MapCardImageUpload.vue";
+import MapCardRating from "./MapCardRating.vue";
+import MapCardLikes from "./MapCardLikes.vue";
+import MapCardEval from "./MapCardEval.vue";
+import MapCardEstateInfo from "./MapCardEstateInfo.vue";
 
 export default {
   components: {
     MapCardImageUpload,
+    MapCardRating,
+    MapCardLikes,
+    MapCardEval,
+    MapCardEstateInfo,
   },
   computed: {
     ...mapGetters({
---
+++ frontend/src/components/Map/MapCardEval.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-btn class="ma-2" color="deep-orange" x-small outlined rounded>
+      <v-icon left x-small>
+        fas fa-edit
+      </v-icon>
+      평가하기
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
