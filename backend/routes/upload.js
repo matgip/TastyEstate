e926e3c837ee5551d35c670aeef476746c2cbd60
junhong91 <junhong199@gmail.com>
2022-02-12 14:36:39 +0900

    Show default Images if images are not uploaded

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -6,16 +6,18 @@ const router = express.Router();
 const fs = require("fs");
 const path = require("path");
 
+const uploadRootPath = path.join(__dirname, "..", "uploads");
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const uploadPath = path.join(__dirname, "..", "uploads", req.params.id);
+    const uploadPath = path.join(uploadRootPath, req.params.id);
     if (!fs.existsSync(uploadPath)) {
       fs.mkdirSync(uploadPath);
     }
     cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
-    const uploadPath = path.join(__dirname, "..", "uploads", req.params.id);
+    const uploadPath = path.join(uploadRootPath, req.params.id);
     fs.readdir(uploadPath, function (err, files) {
       if (err) throw err;
       const filename = (files.length + 1).toString();
@@ -24,6 +26,18 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage: storage });
+
+router.get("/:id", function (req, res, next) {
+  const uploadPath = path.join(uploadRootPath, req.params.id, req.query.image);
+  fs.access(uploadPath, fs.F_OK, (err) => {
+    if (err) {
+      const defaultImgPath = path.join(uploadRootPath, "default.png");
+      res.sendFile(defaultImgPath);
+    } else {
+      res.sendFile(uploadPath);
+    }
+  });
+});
 
 router.post("/:id", upload.single("file"), function (req, res, next) {
   console.log("UPLOAD " + req.file.filename);
@@ -34,7 +48,7 @@ router.put("/:id", function (req, res, next) {
   let response = {};
   if (req.body && req.body.my_key && req.body.filename) {
     console.log("UPDATE UPLOAD " + req.body.my_key);
-    const uploadPath = path.join(__dirname, "..", "uploads", req.params.id);
+    const uploadPath = path.join(uploadRootPath, req.params.id);
     fs.rename(path.join(uploadPath, req.body.my_key), path.join(uploadPath, req.body.filename), (err) => {
       if (err) throw err;
       console.log("Rename complete!", req.body.my_key, req.body.filename);
@@ -49,7 +63,7 @@ router.delete("/:id", function (req, res, next) {
   let response = {};
   if (req.body && req.body.my_key) {
     console.log("DELETE UPLOAD " + req.body.my_key);
-    const uploadPath = path.join(__dirname, "..", "uploads", req.params.id);
+    const uploadPath = path.join(uploadRootPath, req.params.id);
     fs.unlink(path.join(uploadPath, req.body.my_key), (err) => {
       if (err) throw err;
       console.log(req.body.my_key + " was deleted");
--- frontend/src/components/Card/CardImages.vue
+++ frontend/src/components/Card/CardImages.vue
@@ -2,13 +2,8 @@
   <div>
     <v-card-text>
       <v-row>
-        <v-col v-for="n in 3" :key="n" class="d-flex child-flex" cols="4">
-          <v-img
-            :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-            :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-            aspect-ratio="1"
-            class="grey lighten-2"
-          >
+        <v-col v-for="i in images" ref="images" :key="i" class="d-flex child-flex" cols="4">
+          <v-img v-bind:src="getImageURL(estateID, i)" aspect-ratio="1" class="grey lighten-2">
             <template v-slot:placeholder>
               <v-row class="fill-height ma-0" align="center" justify="center">
                 <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
@@ -24,11 +19,17 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      images: [1, 2, 3, 4, 5, 6],
+    };
   },
   props: {
     estateID: String,
   },
-  methods: {},
+  methods: {
+    getImageURL(estateID, imgNum) {
+      return `http://localhost:3000/upload/${estateID}?image=${imgNum}`;
+    },
+  },
 };
 </script>
