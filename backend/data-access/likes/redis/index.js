c3566e3708161ebde6ddec7f48d325cbd15c6de9
junhong91 <junhong199@gmail.com>
2022-02-21 08:18:17 +0900

    handle already added likes

--- backend/data-access/likes/redis/index.js
+++ backend/data-access/likes/redis/index.js
@@ -2,6 +2,10 @@
 
 const client = require("../../../db-client/redis/client");
 
+const SADD_SUCCESS = 0;
+const ALREADY_ADDED = 1;
+const SADD_FAILED = 2;
+
 const getLikes = async (realEstateID) => {
   let result;
   await client.connect();
@@ -16,18 +20,29 @@ const getLikes = async (realEstateID) => {
   return { likes: result };
 };
 
+const int2Str = (cmdInt) => {
+  if (cmdInt === ALREADY_ADDED) return "already-added";
+  if (cmdInt === SADD_FAILED) return "failed";
+  return "success";
+};
+
 const addLikes = async (realEstateID, userID) => {
   let result;
   await client.connect();
 
   try {
-    result = await client.SADD(`likes:${realEstateID}`, `users:${userID}`);
+    const alreadyAdded = await client.SISMEMBER(`likes:${realEstateID}`, `users:${userID}`);
+    if (alreadyAdded) {
+      client.quit();
+      return { cmd_result: int2Str(ALREADY_ADDED) };
+    }
+
+    result = (await client.SADD(`likes:${realEstateID}`, `users:${userID}`)) === 1 ? SADD_SUCCESS : SADD_FAILED;
   } catch (err) {
     console.log(`SADD user command failed: ${err.message}`);
   }
   await client.quit();
-  // Return : 1 => success / 0 => failed
-  return { cmd_result: result };
+  return { cmd_result: int2Str(result) };
 };
 
 module.exports = {
