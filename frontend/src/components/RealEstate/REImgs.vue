a48c54812f57c62db7b37eb6722e83d43108a462
junhong91 <junhong199@gmail.com>
2022-03-27 17:06:37 +0900

    move img to carousel

--- frontend/src/components/RealEstate/REImgs.vue
+++ frontend/src/components/RealEstate/REImgs.vue
@@ -1,15 +1,9 @@
 <template>
   <div>
     <v-row>
-      <v-col v-bind="colProps" v-for="i in images" :key="i">
-        <v-img v-bind="imgProps" :src="getImgURL(estateID, i)">
-          <template #placeholder>
-            <v-row v-bind="rowProps">
-              <v-progress-circular v-bind="progressProps" />
-            </v-row>
-          </template>
-        </v-img>
-      </v-col>
+      <v-carousel>
+        <v-carousel-item v-for="i in images" :key="i" :src="getImgURL(estateID, i)" v-bind="carouselProps" />
+      </v-carousel>
     </v-row>
   </div>
 </template>
@@ -18,22 +12,9 @@
 export default {
   data: () => ({
     images: [1, 2, 3, 4, 5, 6],
-    colProps: {
-      class: "d-flex child-flex",
-      cols: "2",
-    },
-    rowProps: {
-      class: "fill-height ma-0",
-      align: "center",
-      justify: "center",
-    },
-    imgProps: {
-      class: "grey lighten-2",
-      "aspect-ratio": "1",
-    },
-    progressProps: {
-      indeterminate: true,
-      color: "grey lighten-5",
+    carouselProps: {
+      transition: "fade-transition",
+      "reverse-transition": "fade-transition",
     },
   }),
   props: {
