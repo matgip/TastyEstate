378d986a8644513d0d8e12def4a26a5a9260314f
junhong91 <junhong199@gmail.com>
2022-05-14 17:37:10 +0900

    Revert merge: Map erased when route path goto /login and back to home

---
+++ frontend/src/api/map/kakao2.js
@@ -0,0 +1,169 @@
+const normalMarkerImage = require("@/assets/images/marker_selected.png");
+const selectedMarkerImage = require("@/assets/images/marker.png");
+const imgSize = { width: 40, height: 45 };
+
+const SCAN_MIN_LEVEL = 4;
+const CLUSTER_MIN_LEVEL = 3;
+const SCANNED = 1;
+
+class KakaoMap {
+  places = new Map();
+  map = null;
+
+  selectedMarker = null; // 클릭한 마커를 담을 변수
+
+  mount() {
+    if (window.kakao && window.kakao.maps) {
+      this.initMap();
+    } else {
+      const script = document.createElement("script");
+      /* global kakao */
+      script.onload = () => kakao.maps.load(this.initMap);
+      script.src = `${process.env.VUE_APP_MAP_LIB_URL}?autoload=false&appkey=${process.env.VUE_APP_KAKAO_JAVASCRIPT_KEY}&libraries=services,clusterer,drawing`;
+      document.head.appendChild(script);
+    }
+
+    return this;
+  }
+
+  initMap = () => {
+    const container = document.getElementById("mapview");
+    const options = {
+      center: new kakao.maps.LatLng(37.2579324408187, 127.059981890576),
+      level: SCAN_MIN_LEVEL,
+    };
+
+    this.map = new kakao.maps.Map(container, options);
+
+    this.placeSearch = new kakao.maps.services.Places(this.map);
+    this.markerCluster = new kakao.maps.MarkerClusterer({
+      map: this.map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
+      averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+      minLevel: CLUSTER_MIN_LEVEL, // 클러스터 할 최소 지도 레벨
+    });
+
+    // selectedInfowindow 는 오직 1개만 존재해야 되므로 this에 등록
+    this.selectedInfowindow = new kakao.maps.InfoWindow({});
+
+    const imgMarkerSize = new kakao.maps.Size(imgSize.width, imgSize.height);
+    this.normalImage = new kakao.maps.MarkerImage(normalMarkerImage, imgMarkerSize);
+    this.selectedImage = new kakao.maps.MarkerImage(selectedMarkerImage, imgMarkerSize);
+
+    kakao.maps.event.addListener(this.map, "zoom_changed", this.scan);
+    kakao.maps.event.addListener(this.map, "dragend", this.scan);
+    kakao.maps.event.addListener(this.map, "center_changed", this.scan);
+  };
+
+  scan = () => {
+    const lvl = this.map.getLevel();
+    if (lvl >= this.SCAN_MIN_LEVEL) return;
+
+    const { lat, lng } = this._getRoundedCenter();
+
+    for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        const x = (lng + j / 100).toFixed(2);
+        const y = (lat + i / 100).toFixed(2);
+        if (this._isScanned(y, x)) continue;
+        this._cacheLatLng(y, x);
+        this.placeSearch.categorySearch("AG2", this._callback, { x: x, y: y, radius: 300 }); // redius 710 will cover all boundary
+      }
+    }
+  };
+
+  PinPlace(place) {
+    this.addMarker(place);
+    this.map.panTo(new kakao.maps.LatLng(place.y, place.x));
+    kakao.maps.event.trigger(place.marker, "click");
+  }
+
+  getCenter() {
+    const latlng = this.map.getCenter();
+    return { y: latlng.getLat(), x: latlng.getLng() };
+  }
+
+  _getRoundedCenter() {
+    const latlng = this.map.getCenter();
+    const lat = Math.round(latlng.getLat() * 100) / 100;
+    const lng = Math.round(latlng.getLng() * 100) / 100;
+    return { lat: lat, lng: lng };
+  }
+
+  _isScanned(lat, lng) {
+    if (this.cachedLatLng === undefined) return false;
+    if (this.cachedLatLng[lat] === undefined) return false;
+    return this.cachedLatLng[lat][lng] === SCANNED;
+  }
+  _cacheLatLng(lat, lng) {
+    this.cachedLatLng ??= new Array();
+    this.cachedLatLng[lat] ??= new Array();
+    this.cachedLatLng[lat][lng] = SCANNED;
+  }
+
+  _callback = (places, status, pagination) => {
+    if (status === kakao.maps.services.Status.OK) {
+      for (let p of places) {
+        this.addMarker(p);
+      }
+    }
+    if (pagination.hasNextPage) pagination.nextPage();
+  };
+
+  zoomIn = () => {
+    this.map.setLevel(this.map.getLevel() - 1);
+  };
+
+  zoomOut = () => {
+    this.map.setLevel(this.map.getLevel() + 1);
+  };
+
+  addMarker = (place) => {
+    if (this.places.has(place.id)) {
+      return;
+    }
+    const marker = new kakao.maps.Marker({
+      position: new kakao.maps.LatLng(place.y, place.x),
+      image: this.normalImage,
+      clickable: true,
+    });
+    this.markerCluster.addMarker(marker);
+
+    const mouseoverContent = '<div style="padding:2px;">' + place.place_name + "</div>";
+    const mouseoverInfowindow = new kakao.maps.InfoWindow({ content: mouseoverContent });
+
+    const selectedContent = '<div style="padding:5px;font-size:12px;">' + place.place_name + "</div>";
+
+    kakao.maps.event.addListener(marker, "mouseover", () => {
+      if (!this.selectedMarker || this.selectedMarker !== marker) {
+        mouseoverInfowindow.open(this.map, marker);
+      }
+    });
+    kakao.maps.event.addListener(marker, "mouseout", () => {
+      mouseoverInfowindow.close();
+    });
+    kakao.maps.event.addListener(marker, "click", () => {
+      if (!this.selectedMarker || this.selectedMarker !== marker) {
+        !!this.selectedMarker && this.selectedMarker.setImage(this.normalImage);
+        marker.setImage(this.selectedImage);
+        mouseoverInfowindow.close();
+        this.selectedInfowindow.setContent(selectedContent);
+        this.selectedInfowindow.open(this.map, marker);
+        // TODO : notify click event to store
+        this.notifyAgencyClicked(place);
+        this.selectedMarker = marker;
+      }
+    });
+
+    place.marker = marker;
+    this.places.set(place.id, place);
+  };
+
+  setOnClickAgencyListener(listener) {
+    this.onClickAgency = listener;
+  }
+  notifyAgencyClicked(place) {
+    this.onClickAgency(place);
+  }
+}
+
+export default new KakaoMap();
--- frontend/src/components/Map/MapKakao.vue
+++ frontend/src/components/Map/MapKakao.vue
@@ -2,20 +2,14 @@
   <div id="mapview">
     <!-- 지도 확대, 축소 컨트롤 div 입니다 -->
     <div class="custom_zoomcontrol radius_border">
-      <span @click="zoomIn"><i class="fa-solid fa-plus"></i></span>
-      <span @click="zoomOut"><i class="fa-solid fa-minus"></i></span>
+      <span @click="kakaoMap.zoomIn()"><i class="fa-solid fa-plus"></i></span>
+      <span @click="kakaoMap.zoomOut()"><i class="fa-solid fa-minus"></i></span>
     </div>
   </div>
 </template>
 
 <script>
-import MapKakao from "@/api/map/kakao";
-
-import { mapGetters } from "vuex";
-
-const imgSelected = require("@/assets/images/marker_selected.png");
-const imgMarker = require("@/assets/images/marker.png");
-const imgSize = { width: 40, height: 45 };
+import kakaoMap from "@/api/map/kakao2";
 
 export default {
   data() {
@@ -24,65 +18,17 @@ export default {
     };
   },
 
-  async mounted() {
-    await this.initMap();
+  mounted() {
+    this.map = kakaoMap.mount();
+    kakaoMap.setOnClickAgencyListener(this.onClickAgency);
 
     this.$store.commit("UPDATE_MAP", this.map);
-
-    this.$store.subscribe((mutation) => {
-      if (mutation.type == "UPDATE_ESTATE") {
-        const estate = this.estate;
-        if (Object.keys(estate).length === 0) return;
-
-        const marker = this.addMarker({ place: estate, isSelected: true });
-        this.addClickHandler(marker, estate);
-        this.moveTo(estate.y, estate.x);
-        this.scanEstate();
-      }
-    });
-  },
-
-  computed: {
-    ...mapGetters({
-      estate: "GET_ESTATE",
-    }),
   },
 
   methods: {
-    async initMap() {
-      if (this.map) return;
-
-      try {
-        const map = new MapKakao();
-        await map.mount("mapview", { imgMarker, imgSelected, imgSize });
-        this.map = map;
-      } catch (err) {
-        console.error(err);
-      }
-    },
-
-    addMarker(markerEntity) {
-      return this.map.addMarker(markerEntity);
-    },
-
-    addClickHandler(marker, estate) {
-      this.map.onMarkerClicked(marker, estate);
-    },
-
-    scanEstate() {
-      this.map.scan();
-    },
-
-    moveTo(lat, lng) {
-      this.map.moveTo(lat, lng);
-    },
-
-    zoomIn() {
-      this.map.zoomIn();
-    },
-
-    zoomOut() {
-      this.map.zoomOut();
+    onClickAgency(place) {
+      console.log("on click");
+      this.$store.dispatch("updateAgency", place);
     },
   },
 };
